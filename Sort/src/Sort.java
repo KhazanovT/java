8d3bf3f1bcd1a97cf437e652ab5a1817d7235819
KhazanovT <khazanov.asm1504@asugubkin.ru>
2016-06-01 15:29:45 +0300

    3.3

--- Sort/src/Sort.java
+++ Sort/src/Sort.java
@@ -14,12 +14,58 @@ public static String textString;
 public static List<Item> listItem;
 
 static class Item {
-	  String key;
-	  String value;
+	  private String key;
+	  private String value;
 	 public Item(String v) {
-	    value = v;
-	    key = v.toLowerCase(); 
+	    this.value = v;
+	    this.key = v.toLowerCase(); 
 	  }
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return value;
+	}
+	 
 	}
 
 	public static void readText(String way){
@@ -48,7 +94,7 @@ static class Item {
 		 }
 	}
 	public static void main(String[] args){
-		System.out.println("¬ведите путь к файлу:");
+		System.out.println("¬ведите путь к файлу (res/text.txt):");
 		Scanner way = new Scanner(System.in);
 		readText(way.nextLine());
 		Collections.sort(listItem, new Comparator<Item>() {
